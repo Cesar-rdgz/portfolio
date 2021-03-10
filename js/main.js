b0d35da91cd727c0a254e06ad52eea350afa2f1c
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2021-03-09 19:21:30 -0600

    making improvements for advertise

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let slides = document.querySelectorAll('#slider .slide-content');
-const arrowLeft = document.getElementById('arrow-left');
-const arrowRight = document.getElementById('arrow-right');
+const arrowLeft = document.querySelectorAll('.arrow-left');
+const arrowRight = document.querySelectorAll('.arrow-right');
 let current = 0;
 
 function reset(){
@@ -14,34 +14,38 @@ function init(){
     slides[0].style.display = 'flex';
 }
 
-arrowLeft.addEventListener('click', () => {
-    if(current === 0){
-        reset();
-        current = slides.length - 1;
-        slides[current].style.display = "flex";
-        slides[current].classList.add('animateLeft');
-    }else {
-        reset();
-        current--;
-        slides[current].style.display = "flex";
-        slides[current].classList.add('animateLeft');
-    }
-
+arrowLeft.forEach(left => {
+    left.addEventListener('click', () => {
+        if(current === 0){
+            reset();
+            current = slides.length - 1;
+            slides[current].style.display = "flex";
+            slides[current].classList.add('animateLeft');
+        }else {
+            reset();
+            current--;
+            slides[current].style.display = "flex";
+            slides[current].classList.add('animateLeft');
+        }
+    
+    });
 });
 
-arrowRight.addEventListener('click', () => {
-    if(current === slides.length -1){
-        reset();
-        current = 0;
-        slides[current].style.display = "flex";
-        slides[current].classList.add('animateRight');
-    }else {
-        reset();
-        current++;
-        slides[current].style.display = "flex";
-        slides[current].classList.add('animateRight');
-    }
-
+arrowRight.forEach(right => {
+    right.addEventListener('click', () => {
+        if(current === slides.length -1){
+            reset();
+            current = 0;
+            slides[current].style.display = "flex";
+            slides[current].classList.add('animateRight');
+        }else {
+            reset();
+            current++;
+            slides[current].style.display = "flex";
+            slides[current].classList.add('animateRight');
+        }
+    
+    });
 });
 
 init();
